a259972b8b87d47a8a8c76d017ca0651c8cff09e
HudGuedes <hudsonmende@hotmail.com>
2024-03-25 17:23:15 -0300

    Criado os controllers de products e skus e ajustado as rotas

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  def index
+    @products = Product.all
+    render json: @products, include: :skus
+  end
+
+  def show
+    render json: @product, include: :skus
+  end
+
+  def edit
+    render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, include: :skus, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:slug, :name, :description, :manufacturer, :active, skus_attributes: [:id, :code, :name, :stock, :price_list, :listing_price, :_destroy])
+  end
+end
+    
---
+++ app/controllers/skus_controller.rb
@@ -0,0 +1,47 @@
+class SkusController < ApplicationController
+	before_action :set_product
+	before_action :set_sku, only: [:show, :update, :destroy]
+
+	def index
+		@skus = @product.skus
+		render json: @skus
+	end
+
+	def show
+		render json: @sku
+	end
+
+	def edit
+		render json: @sku
+	end
+
+	def create
+	end
+
+	def update
+		if @sku.update(sku_params)
+			render json: @sku
+		else
+			render json: @sku.errors, status: :unprocessable_entity
+		end
+	end
+
+	def destroy
+		@sku.destroy
+	end
+
+	private
+
+	def set_product
+		@product = Product.find(params[:product_id])
+	end
+
+	def set_sku
+		@sku = @product.skus.find(params[:id])
+	end
+
+	def sku_params
+		params.require(:sku).permit(:code, :name, :stock, :price_table, :price_listing)
+	end
+end
+    
